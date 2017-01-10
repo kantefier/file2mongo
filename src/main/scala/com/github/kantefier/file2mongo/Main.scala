2c968cc29776c97ed955a7ab08722b33e9c28931
stroki <kinebogin@gmail.com>
2017-01-11 01:35:21 +0300

    making chunks by userid from input data

--- src/main/scala/com/github/kantefier/file2mongo/Main.scala
+++ src/main/scala/com/github/kantefier/file2mongo/Main.scala
@@ -3,17 +3,14 @@ package com.github.kantefier.file2mongo
 import scalaz._
 import Scalaz._
 import scalaz.stream._
-import scalaz.concurrent._
+import scalaz.concurrent.Task
 
 object Main {
 	def main(args: Array[String]): Unit = {
 		println("Hello, world!")
 	}
 
-	def TODORename(filePath: String) = {
-		val fileStream: Process[Task, String] = io.linesR(filePath)
-//		fileStream.map()
-	}
+	def fileToChunks(filePath: String): Process[Task, Vector[String]] =
+		io.linesR(filePath) |> process1.chunkBy2((x, y) => x.takeWhile(_ != '\t') === y.takeWhile(_ != '\t'))
 
-//	def readUserLines()
 }
