8f53deefe3e3f8adad98cdbce6311f890a9a60bd
stroki <kinebogin@gmail.com>
2017-01-23 17:44:02 +0300

    command line args, a bit of err handling in Main, a few more comments

--- src/main/scala/com/github/kantefier/file2mongo/Main.scala
+++ src/main/scala/com/github/kantefier/file2mongo/Main.scala
@@ -22,16 +22,24 @@ import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, ExecutionContext, Future, Promise}
 
 object Main {
-    def main(args: Array[String]): Unit = {
+    def main(args: Array[String]): Unit = Try {
         val argMap = argsToMap(args)
-        argMap.get("-dataset") match {
-            case Some(path) =>
-                val system = ActorSystem()
-                val mat = ActorMaterializer()
-                finalPipeline(path).run()(mat).onComplete(_ => system.terminate())(mat.executionContext)
+        val datasetPath = argMap.get("-dataset")
+        val db = argMap.get("-db")
+        val coll = argMap.get("-coll")
 
-            case _ => println("Not used correctly. Doing nothing.")
-        }
+        require(datasetPath.isDefined, """Argument -dataset has to be specified. Example: -dataset=D:\dataset\datasetFile""")
+        require(db.isDefined, "Argument -db has to be specified. Example: -db=relevance")
+        require(coll.isDefined, "Argument -coll has to be specified. Example: -coll=users")
+
+        val system = ActorSystem()
+        val mat = ActorMaterializer()(system)
+        finalPipeline(datasetPath.get, db.get, coll.get).run()(mat).onComplete(_ => system.terminate())(mat.executionContext)
+    } recover {
+        case argEx: IllegalArgumentException => println(argEx.getMessage)
+        case ex: Throwable =>
+            println("Something went wrong, my friend...")
+            ex.printStackTrace()
     }
 
     /**
@@ -45,14 +53,15 @@ object Main {
 
     /**
       * Assembled streaming pipeline
+      * Returns a complete pipeline that reads, parses and commits and dataset to MongoDB
       */
-    def finalPipeline(filePath: String) = FileIO.
+    def finalPipeline(filePath: String, dbName: String, collName: String) = FileIO.
       fromPath(Paths.get(filePath), chunkSize = 1000).
       via(Framing.delimiter(ByteString("\n"), 2000, allowTruncation = true)).
       map(_.utf8String).
       via(Flow.fromGraph(new UserLinesCollector)).
       via(linesToDocument).
-      to(Sink.fromGraph(MongoCommitSink("relevance", "test")))
+      to(Sink.fromGraph(MongoCommitSink(dbName, collName)))
 
     /**
      * Flow from lines to a Mongo Document, containing parsed user info
@@ -62,23 +71,20 @@ object Main {
 
 
     /**
-     *  Parse a Mongo Document from user lines
-     */
-    def userDocFromLines(userLines: List[String]): Option[BSONDocument] = {
+      * Parses single user line into tuple (ArtistName, PlaysCount)
+      */
+    def parseSingleArtist(userLine: String): Try[(String, Int)] = {
         val userId = "[0-9a-z]{40}"
         val singleUserLine = raw"""($userId)\s+(.*)""".r
         val artistMbId = "[0-9a-z]{8}-[0-9a-z]{4}-[0-9a-z]{4}-[0-9a-z]{4}-[0-9a-z]{12}"
         val artistWithMbid = raw"""($artistMbId)\s+(.*)""".r
-
-        val linesCount = userLines.length
-        val userIdent = userLines.head.split("\\s").head
 
         def parseArtistInfo(artistInfo: String): Try[(String, Int)] = {
             val tokenized = artistInfo.split("\\s")
             Try(tokenized.init.mkString(" ").trim -> tokenized.last.toInt)
         }
 
-        def parseSingleArtist(userLine: String): Try[(String, Int)] = userLine match {
+        userLine match {
             case singleUserLine(_, artistInfo) =>
                 artistInfo match {
                     case artistWithMbid(_, artistInfo) => parseArtistInfo(artistInfo)
@@ -86,6 +92,14 @@ object Main {
                 }
             case failedLine => Failure(new Exception(s"Couldn't parse line: <<$failedLine>>"))
         }
+    }
+
+    /**
+     *  Parse a Mongo Document from user lines
+     */
+    def userDocFromLines(userLines: List[String]): Option[BSONDocument] = {
+        val linesCount = userLines.length
+        val userIdent = userLines.head.split("\\s").head
 
         val artistAndPlays: List[(String, Int)] = userLines.flatMap(line => parseSingleArtist(line).fold(_ => List.empty, List.apply(_)))
 
@@ -106,11 +120,12 @@ object Main {
         override def createLogicAndMaterializedValue(inheritedAttributes: Attributes): (GraphStageLogic, Future[Done]) = {
             val finishPromise = Promise[Done]()
             val logic = new GraphStageLogic(shape) {
-
                 var driver: MongoDriver = _
                 var connection: MongoConnection = _
 
-                //open client session
+                /**
+                  * Inits Mongo driver and connection
+                  */
                 override def preStart(): Unit = MongoConnection.parseURI("mongodb://localhost") match {
                     case Success(parsedUri) =>
                         println("MongoDriver init")
@@ -121,7 +136,9 @@ object Main {
                         failStage(ex)
                 }
 
-                //close client session
+                /**
+                  * Close driver and all connections
+                  */
                 override def postStop(): Unit = {
                     println("MongoSink: signalling about completetion")
                     finishPromise.success(Done)
@@ -130,21 +147,23 @@ object Main {
                     println("Closed MongoDriver")
                 }
 
-                def errPrint(wr: WriteResult): Unit = if (!wr.ok) {
-                    println(wr.writeErrors.map(_.errmsg).mkString(";"))
-                }
-
                 setHandler(in, new InHandler {
+                    /**
+                      * Receives a document and commits it
+                      * Awaits for the Future to complete, then pulls another doc
+                      */
                     override def onPush() = {
                         val doc = grab(in)
                         implicit val ec = materializer.executionContext
                         val dbAction = for {
                             db <- connection.database(database)
                             insertRes <- db.collection[BSONCollection](collection).insert(doc)
-                            _ <- Future(errPrint(insertRes))
+                            _ <- Future(if (!insertRes.ok) {
+                                println(insertRes.writeErrors.map(_.errmsg).mkString(";"))
+                            })
                         } yield ()
                         Await.ready(dbAction, Duration("2 seconds"))
-                        pull(in)
+                        if(!hasBeenPulled(in)) pull(in)
                     }
                 })
             }
@@ -152,6 +171,9 @@ object Main {
         }
     }
 
+    /**
+      * Collects input Strings into a List[String] based on the built-in criteria
+      */
     class UserLinesCollector extends GraphStage[FlowShape[String, List[String]]] {
         val in: Inlet[String] = Inlet("inLine")
         val out: Outlet[List[String]] = Outlet("outLines")
